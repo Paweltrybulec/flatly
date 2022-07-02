f76d3519036d559b51960ef437b9e0203431aad6
pawel <paweltrybulec@gmail.com>
2022-07-02 22:41:03 +0200

    add validation

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,7 @@
 class Price < ApplicationRecord
   belongs_to :offer
+
+  validates :lent_price, presence: true, numericality: { greater_than: 0 }
+  validates :rent_price, presence: true, numericality: { greater_than: 0 }
+  validates :media_price, presence: true, numericality: { greater_than: 0 }
 end
