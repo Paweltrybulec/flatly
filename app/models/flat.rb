ff7a0bf8c9d7c54c68014dcfa4591c8249d1aa62
pawel <paweltrybulec@gmail.com>
2022-07-02 23:04:00 +0200

    add flat images uploading

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,6 +3,8 @@ class Flat < ApplicationRecord
   belongs_to :address
   has_many :offers
 
+  has_many_attached :images
+
   validates :description, presence: true
   validates :square_footage, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
